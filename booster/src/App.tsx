892144f8b44adc1f9e6d34ddace61bdceab9e022
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2020-06-04 11:38:26 -0300

    feat: Home Page

---
+++ booster/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+import Home from './pages/Home';
+
+const App: React.FC = () => (
+  <>
+    <GlobalStyle />
+    <Home />
+  </>
+);
+
+export default App;
---
+++ booster/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const CreatePoint: React.FC = () => {
+  return (
+    <Container>
+      <h1>Olá</h1>
+    </Container>
+  );
+};
+
+export default CreatePoint;
